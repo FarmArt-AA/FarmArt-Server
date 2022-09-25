b3a4786533f4feb1a67a52ee0336f41c2f95e87b
JunHo KANG <66174425+JunHo-YH@users.noreply.github.com>
2022-09-25 11:32:34 +0900

    init
    init

--- src/main/java/cmc/farmart/FarmArtApplication.java
+++ src/main/java/cmc/farmart/FarmArtApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FarmartApplication {
+public class FarmArtApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FarmartApplication.class, args);
+        SpringApplication.run(FarmArtApplication.class, args);
     }
+
 
 }
