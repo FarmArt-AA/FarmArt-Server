84b0754fe5312f4e4e36dbf5e229aeeb465033aa
JunHo KANG <66174425+JunHo-YH@users.noreply.github.com>
2022-09-25 11:08:08 +0900

    init
    init

---
+++ src/main/java/cmc/farmart/FarmartApplication.java
@@ -0,0 +1,13 @@
+package cmc.farmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cmc/farmart/FarmartApplicationTests.java
@@ -0,0 +1,13 @@
+package cmc.farmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
